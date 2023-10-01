4664d6a21728ca029bfecad5210675bf4932179b
Wilson <wilsonyang315@gmail.com>
2023-10-01 10:00:39 +0800

    create prompt_templates.py, change file directory

--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from prompt_templates import fact_prompt, code_prompt
 import re
 
 
@@ -16,16 +17,7 @@ class FactQuestionHandler(ChatboxHandler):
     def __init__(self, llm):
         super().__init__(llm)
         self.fact_prompt = ChatPromptTemplate.from_template(
-        """这是一个寻求事实的问题。为了获得更准确和有用的答案，请执行以下操作：
-
-        1. 请简化问题，使其尽可能清晰和精炼。
-        2. 请检查问题中的语法和拼写错误，确保问题表达准确。
-        
-        原来的问题如下：
-        {user_input}
-        
-        修改的问题如下：
-        """
+        fact_prompt()
     )
     def handle_input(self, user_input):
         # Logic to handle fact-based questions
@@ -43,15 +35,7 @@ class CodeQuestionHandler(ChatboxHandler):
     def __init__(self, llm) -> None:
         super().__init__(llm)
         self.code_prompt = ChatPromptTemplate.from_template(
-        """这是一个有关编程的问题。为了获得更准确和有用的答案，请优化用户的输入，但不要修改具体的代码。
-        1. 如果用户提供代码，清楚地指出哪一段代码有问题。如果用户的代码有误，不要直接修改。
-        2. 用更加清晰的语言描述问题。
-
-        原来的问题如下：
-        {user_input}
-        
-        修改的问题如下：
-        """
+       code_prompt()
         )
 
     def handle_input(self, user_input):
